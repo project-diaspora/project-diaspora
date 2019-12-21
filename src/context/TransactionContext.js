398a99b84f427848d911ec62b7586a31a69f5d30
msrijai <j@js-MacBook-Pro.local>
2019-12-21 12:25:08 -0500

    small comments

--- src/context/TransactionContext.js
+++ src/context/TransactionContext.js
@@ -17,7 +17,7 @@ const getTransactions = (dispatch) => async (walletAddress) => {
   try {
     const response = await etherscanApi.get(`/api?module=account&action=tokentx&contractaddress=${env.DAI.contractAddress}&address=${walletAddress}&sort=desc&apikey=${env.etherscanKey}`);
     const txData = response.data.result.map(txObject => {
-      if (txObject.to.toLowerCase() === walletAddress.toLowerCase()) {
+      if (txObject.to.toLowerCase() === walletAddress.toLowerCase()) { // toLowerCase() because Ethereum addresses exist in either lowercase or checksum format (a mix of upper and lowercase)
         txObject.type = 'credit'
       } else if (txObject.from.toLowerCase() === walletAddress.toLowerCase()) {
         txObject.type = 'debit'
